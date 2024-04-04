c18c12ada23fa83d4832cbfbc7951efba9e416d7
nischup <ahsabbir23@gmail.com>
2024-04-04 16:02:46 +0600

    lifecycle hooks read throw

---
+++ Practice/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from vue
+const msg = ref('Hello world') 
+
+</script>
+
+<template>
+    {{ msg }}
+    <input v-model="msg">
+</template>
--- npm-vue-project/blog/src/views/HomeView.vue
+++ npm-vue-project/blog/src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref } from 'vue'
+
+const data_count = ref(0)
+
+
+function increment() {
+  data_count.value++
+}
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+    <button @click="increment">
+    {{ data_count }}
+  </button>
+
+  <!-- Conditional Rendering -->
+
+  <div v-if="type === 'A'">
+  A
+</div>
+<div v-else-if="type === 'B'">
+  B
+</div>
+<div v-else-if="type === 'C'">
+  C
+</div>
+<div v-else>
+  Not A/B/C
+</div>
+
 </template>
